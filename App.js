b47357671bcd41f11334ed846ffef9e590c3b656
Silvokyda <deseve48@gmail.com>
2024-05-04 19:24:22 +0300

    Rive installation

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import Rive from 'rive-react-native';
 import { StyleSheet, Text, View } from 'react-native';
+
+function RiveDemo() {
+  return <Rive
+      url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+      artboardName="Avatar 1"
+      stateMachineName="avatar"
+      style={{width: 400, height: 400}}
+  />;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RiveDemo />
     </View>
   );
 }
